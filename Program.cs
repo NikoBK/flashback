cc06b8241308d1e8d278d314fcbead89164e6c68
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-11-09 23:08:13 +0100

    high dpi support

--- Program.cs
+++ Program.cs
@@ -1,16 +1,31 @@
+using System.Runtime.InteropServices;
+
 namespace flashback_emulator
 {
     internal static class Program
     {
+        [DllImport("Shcore.dll")]
+        static extern int SetProcessDpiAwareness(int PROCESS_DPI_AWARENESS);
+
+        // According to https://msdn.microsoft.com/en-us/library/windows/desktop/dn280512(v=vs.85).aspx
+        private enum DpiAwareness
+        {
+            None = 0,
+            SystemAware = 1,
+            PerMonitorAware = 2
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            SetProcessDpiAwareness((int)DpiAwareness.PerMonitorAware);
+
             Application.Run(new Form1());
         }
     }
